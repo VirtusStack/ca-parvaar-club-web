5551e66f38ef16abc212896f710d87ea5f3ea7af
VirtusStack <payalgupta6403@gmail.com>
2026-01-21 11:12:36 +0530

    Create proximity.php

---
+++ templates/includes/proximity.php
@@ -0,0 +1,43 @@
+<section class="ca-proximity">
+  <div class="ca-proximity-inner">
+
+    <!-- LEFT CONTENT -->
+    <div class="ca-proximity-text">
+      <h2>Club Proximity</h2>
+
+      <p>
+        There are a great many places to visit in Mount Abu for those looking
+        for an escape from the scorching heat of Rajasthan, while also
+        experiencing its royal demeanor.
+      </p>
+
+      <a href="#" class="ca-proximity-view">View All</a>
+    </div>
+
+    <!-- RIGHT IMAGES -->
+    <div class="ca-proximity-images">
+
+      <div class="ca-proximity-card">
+        <img src="assets/images/proximity/brahma-kumari.jpg" alt="Brahma Kumari">
+        <span>Brahma Kumari</span>
+      </div>
+
+      <div class="ca-proximity-card">
+        <img src="assets/images/proximity/ambaji-temple.jpg" alt="Ambaji Temple">
+        <span>Ambaji Temple</span>
+      </div>
+
+      <div class="ca-proximity-card">
+        <img src="assets/images/proximity/dilwara-temple.jpg" alt="Dilwara Jain Temple">
+        <span>Dilwara Jain Temple</span>
+      </div>
+
+      <div class="ca-proximity-card">
+        <img src="assets/images/proximity/nakki-lake.jpg" alt="Nakki Lake">
+        <span>Nakki Lake</span>
+      </div>
+
+    </div>
+
+  </div>
+</section>
