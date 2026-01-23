f059e9d6d423c3b288fee7ddfe4cf71b523df020
VirtusStack <payalgupta6403@gmail.com>
2026-01-23 19:33:13 +0530

    Update footer.php

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -92,6 +92,7 @@ scrollBtn.addEventListener("click", () => {
   });
 });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
