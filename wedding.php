d05271111d8cffe3d197ec1f7a6fee2f891eb815
VirtusStack <payalgupta6403@gmail.com>
2026-01-26 16:27:51 +0530

    Update wedding.php

--- wedding.php
+++ wedding.php
@@ -16,7 +16,44 @@ include __DIR__ . '/templates/includes/header.php';
 <section class="ca-page-title">
   <h1>Wedding</h1>
 <p class="ca-page-subtitle">Spread across 18 acres</p>
+</section>
 
+<section class="ca-conference-wrapper">
+  <div class="ca-conference-container">
+
+    <!-- IMAGE -->
+    <div class="ca-conference-left">
+      <img src="assets/images/club/wedding.png" alt="wedding">
+    </div>
+
+    <!-- CONTENT -->
+    <div class="ca-conference-right">
+
+      <h3 class="ca-conference-title">
+        <span></span> Plan Your Wedding
+      </h3>
+
+      <p>
+       Celebrations are a part of life that mark memorable occasions like Weddings,
+       Engagements, Birthdays or other important rituals of various religious faiths
+       and traditions. CA PARIWAR CLUB has a variety of venues for you to celebrate 
+	with grandeur. Choose the best suited for you depending upon your requirement
+        of size and the budget of your celebrations.
+      </p>
+
+      <strong>Our USP’s:</strong>
+
+      <ul>
+        <li>Bespoke Decor.</li>
+        <li>100% Power back-up.</li>
+        <li>Customized Ambiance.</li>
+        <li>Delectable Cuisines.</li>
+        <li>Unmatched Hospitality.</li>
+        <li>Personalized Service.</li>
+      </ul>
+
+    </div>
+  </div>
 </section>
 
 <?php include __DIR__ . '/templates/includes/proximity.php'; ?>
