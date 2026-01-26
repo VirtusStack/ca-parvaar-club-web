21156090ae327d125872458f0dd592c4d8a9a717
VirtusStack <payalgupta6403@gmail.com>
2026-01-26 19:31:23 +0530

    Update Celebration-time.php

--- Celebration-time.php
+++ Celebration-time.php
@@ -1,8 +1,7 @@
 <?php
-$currentPage = 'amenities';
+$currentPage = basename($_SERVER['PHP_SELF']);
 include __DIR__ . '/templates/includes/header.php';
 ?>
-
 <!-- ===== BANNER ===== -->
 <section class="ca-banner" style="background-image:url('assets/images/amenities/Celebration-time/celebration-banner.jpg')">
   <div class="ca-banner-overlay">
