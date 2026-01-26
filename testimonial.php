db3e3bc7852f998ec2023f95f273e8a47cad3933
VirtusStack <payalgupta6403@gmail.com>
2026-01-26 16:26:26 +0530

    Create testimonial.php

---
+++ testimonial.php
@@ -0,0 +1,349 @@
+<?php
+$currentPage = 'testimonial.php';
+include __DIR__ . '/templates/includes/header.php';
+?>
+
+<!-- ===== PAGE BANNER ===== -->
+<section class="ca-banner" style="background-image:url('assets/images/testimonial/testimonials-banner.jpg')">
+  <div class="ca-banner-overlay">
+    <div class="ca-breadcrumb">
+      <a href="testimonial.php">Home</a> » <strong>Testimonial</strong>
+    </div>
+  </div>
+</section>
+
+<!-- ===== PAGE TITLE ===== -->
+<section class="ca-page-title">
+  <h1>Testimonial</h1>
+ </section>
+
+<section class="ca-testimonial-wrap">
+  <div class="container">
+
+    <!-- ONE ROW -->
+    <div class="row ca-testimonial-row">
+
+      <!-- LEFT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+          <h4>
+          <span class="yellow-line"></span>
+          Mr. Balbir Singh
+	 </h4>
+          <p><strong>Location :</strong> New Delhi</p>
+          <p><strong>Designation :</strong> IT</p>
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+            <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Good</td></tr>
+            <tr><td>Staff</td><td>Very Good</td></tr>
+            <tr><td>Room</td><td>Good</td></tr>
+            <tr><td>Facilities</td><td>Excellent</td></tr>
+            <tr><td>Campus</td><td>Best</td></tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- RIGHT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+	<h4>
+          <span class="yellow-line"></span>
+          Mr. Basir Ahemad Shaikh
+	</h4>
+          <p><strong>Location :</strong> Jaipur</p>
+          <p><strong>Designation :</strong> Assistant General Manager</p>
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+              <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Very Good</td></tr>
+            <tr><td>Staff</td><td>Good</td></tr>
+            <tr><td>Room</td><td>Very Good</td></tr>
+            <tr><td>Facilities</td><td>Excellent</td></tr>
+            <tr><td>Campus</td><td>Excellent</td></tr>
+          </tbody>
+        </table>
+      </div>
+  </div>
+  </div>
+</section>
+
+<section class="ca-testimonial-wrap">
+  <div class="container">
+
+    <!-- TWO ROW -->
+    <div class="row ca-testimonial-row">
+
+        <!-- LEFT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+          <h4>
+          <span class="yellow-line"></span>
+          Mr. Ankit Doshi
+	 </h4>
+          <p><strong>Location :</strong> Mumbai</p>
+          <p><strong>Designation :</strong> Bussiness Man</p>
+
+ 	  <img class="testimonial-img" src="assets/images/testimonial/ankit-doshi-guest.jpg">
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+            <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Excellent</td></tr>
+            <tr><td>Staff</td><td>Very Good & Polite</td></tr>
+            <tr><td>Room</td><td>Very Good</td></tr>
+            <tr><td>Facilities</td><td>Excellent</td></tr>
+            <tr><td>Campus</td><td>Very Good</td></tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- RIGHT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+	<h4>
+          <span class="yellow-line"></span>
+         Mr. Shrikant Bamane
+	</h4>
+          <p><strong>Location :</strong> Mumbai</p>
+          <p><strong>Designation :</strong>-</p>
+	<img class="testimonial-img" src="assets/images/testimonial/shrikant-bamane-guset.jpg">
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+              <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Very Good</td></tr>
+            <tr><td>Staff</td><td>Very Good</td></tr>
+            <tr><td>Room</td><td>Very Good</td></tr>
+            <tr><td>Facilities</td><td>Good</td></tr>
+            <tr><td>Campus</td><td>Good</td></tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<section class="ca-testimonial-wrap">
+  <div class="container">
+
+    <!-- THREE ROW -->
+    <div class="row ca-testimonial-row">
+
+      <!-- LEFT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+          <h4>
+          <span class="yellow-line"></span>
+         Mr. Abhijit biswas
+	 </h4>
+          <p><strong>Location :</strong> Delhi</p>
+          <p><strong>Designation :</strong> Constance Civil Engineer</p>
+         <img class="testimonial-img" src="assets/images/testimonial/abhijit-biswas-guest.jpg">
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+            <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Very Good</td></tr>
+            <tr><td>Staff</td><td>Excellent</td></tr>
+            <tr><td>Room</td><td>Good</td></tr>
+            <tr><td>Facilities</td><td>Good</td></tr>
+            <tr><td>Campus</td><td>Very Good</td></tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- RIGHT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+	<h4>
+          <span class="yellow-line"></span>
+          Mr. Shiv Kumar
+	</h4>
+          <p><strong>Location :</strong> Agra</p>
+          <p><strong>Designation :</strong> Senior Software Engineer (Software Development)</p>
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+              <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Very Good</td></tr>
+            <tr><td>Staff</td><td>Good</td></tr>
+            <tr><td>Room</td><td>Excellent</td></tr>
+            <tr><td>Facilities</td><td>Excellent</td></tr>
+            <tr><td>Campus</td><td>Good</td></tr>
+          </tbody>
+        </table>
+      </div>
+  </div>
+  </div>
+</section>
+
+<section class="ca-testimonial-wrap">
+  <div class="container">
+
+    <!-- FOUR ROW -->
+    <div class="row ca-testimonial-row">
+
+      <!-- LEFT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+          <h4>
+          <span class="yellow-line"></span>
+          Mr. Hargopal Singh
+	 </h4>
+          <p><strong>Location :</strong>Hoshiarpur</p>
+          <p><strong>Designation :</strong> Clerk</p>
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+            <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Very Good</td></tr>
+            <tr><td>Staff</td><td>Excellent</td></tr>
+            <tr><td>Room</td><td>Good</td></tr>
+            <tr><td>Facilities</td><td>Very Good</td></tr>
+            <tr><td>Campus</td><td>Excellent</td></tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- RIGHT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+	<h4>
+          <span class="yellow-line"></span>
+         Mr. Anil Kumar Yadav
+	</h4>
+          <p><strong>Location :</strong> Jaipur</p>
+          <p><strong>Designation :</strong>  Genral Manager (retired) RBI</p>
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+              <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Excellent</td></tr>
+            <tr><td>Staff</td><td>Excellent</td></tr>
+            <tr><td>Room</td><td> Good</td></tr>
+            <tr><td>Facilities</td><td>Good</td></tr>
+            <tr><td>Campus</td><td>Excellent</td></tr>
+          </tbody>
+        </table>
+      </div>
+  </div>
+  </div>
+</section>
+
+<section class="ca-testimonial-wrap">
+  <div class="container">
+
+    <!-- Five ROW -->
+    <div class="row ca-testimonial-row">
+
+      <!-- LEFT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+          <h4>
+          <span class="yellow-line"></span>
+         Mr. Arpit B. Chokshi
+	 </h4>
+          <p><strong>Location :</strong>Vadodara</p>
+          <p><strong>Designation :</strong> Manager</p>
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+            <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Excellent</td></tr>
+            <tr><td>Staff</td><td>Excellent</td></tr>
+            <tr><td>Room</td><td>Good</td></tr>
+            <tr><td>Facilities</td><td> Good</td></tr>
+            <tr><td>Campus</td><td>Good</td></tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- RIGHT TESTIMONIAL -->
+      <div class="col-md-6 ca-testimonial-box">
+        <div class="ca-person">
+	<h4>
+          <span class="yellow-line"></span>
+         Mr. Neeraj Kumar
+	</h4>
+          <p><strong>Location :</strong> Ajmer</p>
+         
+        </div>
+
+        <table class="ca-feedback">
+          <thead>
+              <tr>
+	  <th class="feedback-empty"></th>
+	  <th class="feedback-title">Feedback</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Food</td><td>Good</td></tr>
+            <tr><td>Staff</td><td>Good</td></tr>
+            <tr><td>Room</td><td>Very Good</td></tr>
+            <tr><td>Facilities</td><td>Excellent</td></tr>
+            <tr><td>Campus</td><td>Excellent</td></tr>
+          </tbody>
+        </table>
+      </div>
+  </div>
+  </div>
+</section>
+
